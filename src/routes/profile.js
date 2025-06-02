2f4a994c2df478794ba79f1c4d8ba084e3130bc8
Tannu Saxena <tannusaxena02@gmail.com>
2025-06-02 10:30:13 +0530

    forgot password API

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const profileRouter = express.Router();
 const { userAuthentication } = require("../middlewares/auth.js");
 const { vallidateProfileData } = require("../utils/validation.js");
+const { User } = require("../models/user.js");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
+
 profileRouter.get("/profile/view", userAuthentication, async (req, res) => {
   try {
     const user = req.user;
@@ -33,6 +37,32 @@ profileRouter.patch("/profile/edit", userAuthentication, async (req, res) => {
   }
 });
 
+profileRouter.post("/forgotpassword",userAuthentication,async(req,res)=>{
+    try{
+        //password,new password
+        const{ password,newPassword }=req.body;
+        const passwordHash=req.user.password;
+        const isPasswordValid= await bcrypt.compare(password ,passwordHash);
+        if(!isPasswordValid)
+        {
+            throw new Error("Invalid Existing password");
+        }
+        if(!validator.isStrongPassword(newPassword))
+        {
+            throw new Error("new Password is not strong enough");
+        }
+        if (password === newPassword) 
+          throw new Error("New password must be different from current password.");
+        const updatedPasswordHash = await bcrypt.hash(newPassword, 10);
+        req.user.password=updatedPasswordHash;
+         await req.user.save();        
+        res.send("Password updated successfully");
+    }
+    catch(err)
+    {
+        res.status(400).send("Error "+err.message);
+    }
+})
 module.exports = {
     profileRouter
 };
