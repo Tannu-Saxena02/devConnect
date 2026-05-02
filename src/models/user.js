265a49007f6896e863c5013c3f0e1979eba2cf6d
Tannu saxena <tannusaxena02@gmail.com>
2026-05-02 15:26:00 +0530

    fixes

--- src/models/user.js
+++ src/models/user.js
@@ -84,7 +84,7 @@ const userSchema = new mongoose.Schema(
 );
 userSchema.methods.getJWT = async function () {
   const user = this;
-  const token = await jwt.sign({ _id: user._id }, "DevTinder@1234$", {
+  const token = await jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
     expiresIn: "1d",
   }); 
   return token;
